886956378e2c7c565ffd78453ea06d1e808f497d
nifla-thasni <82957609+nifla-thasni@users.noreply.github.com>
2022-11-08 16:11:44 +0530

    complete

---
+++ gameapp/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import Players from './Components/Players';
+
+function App() {
+  return (
+    <div className="App" >
+       < Players />
+       </div>
+  );
+}
+
+export default App;
---
+++ gameapp/src/Components/Score.js
@@ -0,0 +1,14 @@
+// import React from "react";
+
+// export default function Players({item}) {
+    
+//   return (
+//     <div>
+//       <div className="dice">
+//         <p className="Player1">{item.title}</p>
+//         <img className="img1" src={item.image} />
+//         <p id="scr1">{item.score}</p>
+//       </div>
+//     </div>
+//   );
+// }
